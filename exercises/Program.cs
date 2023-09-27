15f6434d7d10a18366724ddbc0ce7faec21f5c41
Gabriel Neves Oliveira Quintão <gabrieloliveiraquintao@gmail.com>
2023-09-27 08:04:20 -0300

    Update Program.cs

--- exercises/Program.cs
+++ exercises/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int x, y;
+        Console.Write("X:");
+        x = int.Parse(Console.ReadLine());
+        Console.Write("Y:");
+        y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("X + Y = "+ Sum(x,y));
+    }
+
+    public static int Sum(int x, int y)
+    {
+        
+    }
+}
